56750e2e3bda4ad0beea3a8c5a4c7b4fc65b603b
hitka <64646590+hitka@users.noreply.github.com>
2022-11-14 00:26:01 +0300

    Update sakugabooru.php

--- sakugabooru.php
+++ sakugabooru.php
@@ -1,7 +1,6 @@
-<php?
+
+function f(name, seriya){
 $sContent = file_get_contents('https://www.sakugabooru.com/post?tags=bleach+source%3A%23118');
       echo $sContent;
-?>
-function f(name, seriya){
   return "Название: "+name+" Серия: " +seriya;
 }
